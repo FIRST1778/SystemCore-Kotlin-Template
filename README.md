642f5613725aecf6172f59a774cc10c177e6f784
rowan-mcalpin <87948250+rowan-mcalpin@users.noreply.github.com>
2025-08-09 13:03:03 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# SystemCore Kotlin Template
+
+This repository contains a SystemCore FRC project with Kotlin added. Just duplicate the repo,
+change the team number in `.wpilib/wpilib_preferences.json`, and you can begin coding!
---
+++ src/main/kotlin/frc/robot/KotlinUtil.kt
@@ -0,0 +1,10 @@
+package frc.robot
+
+import edu.wpi.first.wpilibj2.command.CommandScheduler
+
+/*
+THIS IS A UTILITY FILE TO CLEAN UP KOTLIN CODE. ROBOT-SPECIFIC CODE SHOULD NOT BE ADDED TO THIS FILE
+ */
+
+val commandScheduler: CommandScheduler
+    get() = CommandScheduler.getInstance()
